5a6adc322778d3e47ee67c38dd32112a93bbe340
leandrohorizon <leandro.mcontato@gmail.com>
2022-10-09 15:25:15 -0300

    Transformando fingerprint em command

--- app/models/fingerprint.py
+++ app/models/fingerprint.py
@@ -1,51 +1,48 @@
 import os
 import cv2
 
-sample = cv2.imread(
-    'SOCOFing/Altered/Altered-Hard/150__M_Right_index_finger_Obl.BMP')
+class FingerPrint():
+    def __init__(self, file):
+        self.input_image = cv2.imread(file)
 
-best_score = 0
+        self.best_score = 0
 
-filename = None
-image = None
-kp1, kp2, mp = None, None, None
+        self.filename = None
+        self.image = None
+        self.keypoints_1 = None
+        self.keypoints_2 = None
+        self.match_points = None
 
-for file in [file for file in os.listdir('SOCOFing/Real')]:
-    fingerprint_image = cv2.imread('SOCOFing/Real/' + file)
-    sift = cv2.SIFT_create()
+    def call(self):
+        file_names = os.listdir('SOCOFing/Real')
+        for filename in file_names:
+            fingerprint_image = cv2.imread(f'SOCOFing/Real/{filename}')
+            sift = cv2.SIFT_create()
 
-    keypoints_1, descriptors_1 = sift.detectAndCompute(sample, None)
-    keypoints_2, descriptors_2 = sift.detectAndCompute(fingerprint_image, None)
+            keypoints_1, descriptors_1 = sift.detectAndCompute(self.input_image, None)
+            keypoints_2, descriptors_2 = sift.detectAndCompute(fingerprint_image, None)
 
-    matches = cv2.FlannBasedMatcher({'algorithm': 1, 'trees': 10},
-                                    {}).knnMatch(descriptors_1, descriptors_2, k=2)
+            matches = cv2.FlannBasedMatcher({'algorithm': 1, 'trees': 10},
+                                            {}).knnMatch(descriptors_1, descriptors_2, k=2)
 
-    match_points = []
+            match_points = []
 
-    for p, q in matches:
-        if p.distance < 0.1 * q.distance:
-            match_points.append(p)
+            for p, q in matches:
+                if p.distance < 0.1 * q.distance:
+                    match_points.append(p)
 
-    keypoints = 0
-    if len(keypoints_1) < len(keypoints_2):
-        keypoints = len(keypoints_1)
-    else:
-        keypoints = len(keypoints_2)
+            keypoints = 0
+            if len(keypoints_1) < len(keypoints_2):
+                keypoints = len(keypoints_1)
+            else:
+                keypoints = len(keypoints_2)
 
-    donow = len(match_points) / keypoints * 100
+            score = len(match_points) / keypoints * 100
 
-    if donow > best_score:
-        best_score = donow
-        filename = file
-        image = fingerprint_image
-        kp1, kp2, mp = keypoints_1, keypoints_2, match_points
-
-print("BEST MATCH: " + str(filename))
-print("SCORE: " + str(best_score))
-
-result = cv2.drawMatches(sample, kp1, image, kp2, mp, None)
-result = cv2.resize(result, None, fx=4, fy=4)
-
-cv2.imshow("Result", result)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+            if score > self.best_score:
+                self.best_score = score
+                self.filename = filename
+                self.image = fingerprint_image
+                self.keypoints_1 = keypoints_1
+                self.keypoints_2 = keypoints_2
+                self.match_points = match_points
