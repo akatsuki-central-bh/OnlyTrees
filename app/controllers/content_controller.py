dbd3c3bbb9a34a017401b3b42e171b9ac9785c0b
leandrohorizon <leandro.mcontato@gmail.com>
2022-10-29 13:27:17 -0300

    Importa model content ao controller e refatora rota de update

--- app/controllers/content_controller.py
+++ app/controllers/content_controller.py
@@ -5,6 +5,7 @@ from werkzeug.exceptions import abort
 
 from .auth_controller import login_required
 from app.database.database import get_db
+from app.models.content import Content
 
 bp = Blueprint('blog', __name__)
 
@@ -45,50 +46,33 @@ def create():
         db.commit()
         return redirect(url_for('blog.index'))
 
-    return render_template('blog/create.html')
+@bp.route('/<int:id>/edit', methods='GET')
+def edit(id):
+    content = Content.find(id)
+    return render_template('blog/update.html', content=content)
 
-def get_post(id, check_author=True):
-    post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
-        (id,)
-    ).fetchone()
-
-    if post is None:
-        abort(404, f"Post id {id} doesn't exist.")
-
-    if check_author and post['author_id'] != g.user['id']:
-        abort(403)
-
-    return post
-
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@bp.route('/<int:id>/update', methods='POST')
 @login_required
 def update(id):
-    post = get_post(id)
+    title = request.form['title']
+    body = request.form['body']
+    error = None
 
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        error = None
+    if not title:
+        error = 'Title is required.'
 
-        if not title:
-            error = 'Title is required.'
+    if error is not None:
+        flash(error)
+    else:
+        db = get_db()
+        db.execute(
+            'UPDATE post SET title = ?, body = ?'
+            ' WHERE id = ?',
+            (title, body, id)
+        )
+        db.commit()
+        return redirect(url_for('blog.index'))
 
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE post SET title = ?, body = ?'
-                ' WHERE id = ?',
-                (title, body, id)
-            )
-            db.commit()
-            return redirect(url_for('blog.index'))
-
-    return render_template('blog/update.html', post=post)
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
