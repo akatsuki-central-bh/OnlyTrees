a96fb5a9259b24f2038fbcd025f3640c39809634
Cauã R. Pereira <pereira.cauaa7@gmail.com>
2022-11-01 19:15:50 -0300

    Feito a lógica para adicionar um novo usuario

--- app/controllers/admin_controller.py
+++ app/controllers/admin_controller.py
@@ -5,6 +5,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from app.models.fingerprint import FingerPrint
+from app.models.user import User
 
 bp = Blueprint('admin', __name__, url_prefix='/admin')
 
@@ -13,7 +14,7 @@ from .auth_controller import login_required
 def admin_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
-        if g.user.role == 1:
+        if g.user.role != 1:
             flash('você não tem permissão para acessar essa página')
             return redirect(url_for('index'))
 
@@ -24,13 +25,17 @@ def admin_required(view):
 @bp.route('/invite', methods=['GET'])
 @admin_required
 def new_user():
-    return render_template('auth/invite.html')
+    users = User.all()
+    return render_template('auth/invite.html', users=users)
 
 @bp.route('/invite', methods=['POST'])
 @admin_required
 def create_user():
     username = request.form['username']
+    role = request.form['perfil']
+    password = "admin123"
     flash(username)
+    User.create(username, password, role, None)
     return render_template('auth/invite.html')
 
 @admin_required
